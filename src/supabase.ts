723357e70c7b177ad1c613ae70c3a95fbaa34049
omegarhovega <112803629+omegarhovega@users.noreply.github.com>
2024-01-13 20:36:22 +0100

    .env adjustment

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.AMPLIFY_SIWA_CLIENT_ID as string;
-const supabaseAnonKey = process.env.AMPLIFY_SIWA_PRIVATE_KEY as string;
+const supabaseUrl = process.env.secrets.AMPLIFY_SIWA_CLIENT_ID as string;
+const supabaseAnonKey = process.env.secrets.AMPLIFY_SIWA_PRIVATE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
