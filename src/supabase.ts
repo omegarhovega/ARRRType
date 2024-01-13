f5e244b1f6fbd0bfaaab6deed086ee7fad9930cd
omegarhovega <112803629+omegarhovega@users.noreply.github.com>
2024-01-13 20:48:46 +0100

    Update .env

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-let secrets = {};
+interface Secrets {
+    AMPLIFY_SIWA_CLIENT_ID?: string;
+    AMPLIFY_SIWA_PRIVATE_KEY?: string;
+}
+
+let secrets: Secrets = {};
 if (process.env.secrets) {
     secrets = JSON.parse(process.env.secrets);
 }
@@ -8,4 +13,6 @@ if (process.env.secrets) {
 const supabaseUrl = secrets.AMPLIFY_SIWA_CLIENT_ID;
 const supabaseAnonKey = secrets.AMPLIFY_SIWA_PRIVATE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL or Anon Key is missing");
+}
