728832bb9cf4f7ff4d9dd71921dc47b3580b6dc8
omegarhovega <112803629+omegarhovega@users.noreply.github.com>
2024-01-14 09:46:59 +0100

    .env update

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,20 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import 'dotenv/config'
-require("dotenv").config();
 
-
-interface Secrets {
-    AMPLIFY_SIWA_CLIENT_ID?: string;
-    AMPLIFY_SIWA_PRIVATE_KEY?: string;
-}
-
-let secrets: Secrets = {};
-if (process.env.secrets) {
-    secrets = JSON.parse(process.env.secrets);
-}
-
-const supabaseUrl = secrets.AMPLIFY_SIWA_CLIENT_ID;
-const supabaseAnonKey = secrets.AMPLIFY_SIWA_PRIVATE_KEY;
+const supabaseUrl = process.env.AMPLIFY_SIWA_CLIENT_ID;
+const supabaseAnonKey = process.env.AMPLIFY_SIWA_PRIVATE_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL or Anon Key is missing");
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import { fileURLToPath, URL } from 'node:url';
-import CompressionPlugin from 'vite-plugin-compression';  // Import the plugin
-import { defineConfig } from 'vite';
+import CompressionPlugin from 'vite-plugin-compression';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    CompressionPlugin({ algorithm: 'gzip' }),  // Add the plugin here
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+//https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue(),
+      CompressionPlugin({ algorithm: 'gzip' }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  }
 });
