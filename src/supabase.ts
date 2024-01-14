655b67d3e8f4fa140cf05609de38b2392baef72b
omegarhovega <112803629+omegarhovega@users.noreply.github.com>
2024-01-14 10:52:34 +0100

    updated supabase variable import

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 
-console.log("Supabase URL:", import.meta.env.VITE_APP_SUPABASE_URL);
-
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL or Anon Key is missing");
 }
